9591cab0b41568148fef0aaba0c53ad8ff43aa7c
Maryam Maleki <61164413+mello-e@users.noreply.github.com>
2024-05-21 15:47:57 +0200

    api added

--- src/index.js
+++ src/index.js
@@ -1,8 +1,24 @@
+function changeWeather(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let temperature = response.data.temperature.current;
+  let cityElement = document.querySelector("#city");
+
+  cityElement.innerHTML = response.data.city;
+  temperatureElement.innerHTML = Math.round(temperature);
+}
+function searchCity(city) {
+  let apiKey = "3f900ea3aatoe1a3dffe6cbab40486fb";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+  axios.get(apiUrl).then(changeWeather);
+}
+
 function changeCity(event) {
   event.preventDefault();
   let newCity = document.querySelector("#search-form-input");
   let cityElement = document.querySelector("#city");
   cityElement.innerHTML = newCity.value;
+
+  searchCity(newCity.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
